8ff92a358556cc58a195414ebc41c86efb8fe52e
iruletheworld <siyu.gao@outlook.com>
2019-10-16 02:02:43 +0100

    purge recursively

--- to_png/purge.py
+++ to_png/purge.py
@@ -18,7 +18,13 @@ __author__  = u'高斯羽 博士 (Dr. Gāo, Sī Yǔ)'
 __version__ = '1.0.0'
 __date__    = '2019-10-15'
 
-list_files = os.listdir(os.getcwd())
+list_files = []
+
+for root, dirnames, filenames in os.walk(os.getcwd()):
+
+    for filename in filenames:
+
+        list_files.append(os.path.join(root, filename))
 
 list_files = [i for i in list_files if not i.endswith(('.bat', '.tex', '.py'))]
 
@@ -26,6 +32,14 @@ list_files = [os.path.join(os.getcwd(), i) for i in list_files]
 
 for i in list_files:
 
-    os.remove(i)
+    try:
+
+        os.remove(i)
+
+    except Exception as e:
+
+        print(repr(e))
+
+        pass
 
 print('Purged!')
