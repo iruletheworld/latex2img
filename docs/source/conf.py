ae8d89fd173fe9674c3dd7a94114a165f1cde122
iruletheworld <siyu.gao@outlook.com>
2019-10-23 19:57:44 +0100

    \setCJKmainfont{AR PL Mingti2L Big5}

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,8 @@ latex_elements = {
     'preamble': r'''
 
     \usepackage{xeCJK}
-    \setCJKmainfont{AR PL KaitiM Big5}
+    % 'AR PL KaitiM Big5' can be used, but turns every main into 楷体, which is the italic equivalent
+    \setCJKmainfont{AR PL Mingti2L Big5}
     \setCJKsansfont{Droid Sans Fallback}
 
     \usepackage{float}
